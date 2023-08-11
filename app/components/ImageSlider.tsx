4847e20a3f99c1f862d5b9f0f4d8825a7bee1772
Surya Jammalamadaka <suryaj@Suryas-MacBook-Pro.local>
2023-08-11 18:06:17 -0400

    Arrows added

--- app/components/ImageSlider.tsx
+++ app/components/ImageSlider.tsx
@@ -6,10 +6,9 @@ export default function ImageSlider({slides}: any){
     
     const [currentIndex, setCurrentIndex] = useState(0)
    
-    
     const src = slides[currentIndex].url;
 
-    const changeSlides = () => {
+    const nextSlide = () => {
         if(currentIndex == slides.length-1){
             setCurrentIndex(0)
         }
@@ -19,12 +18,24 @@ export default function ImageSlider({slides}: any){
         
     }
 
+    const prevSlide = () => {
+        console.log(currentIndex, "current")
+        let length = slides.length;
+        if(currentIndex == 0){
+            setCurrentIndex(length-1)
+        }
+        else{
+            setCurrentIndex(currentIndex-1)
+        }
+    }
+
 
     return (
         
         <div className="coverContainer">
+            <button onClick={prevSlide} className="arrows prevArrow">&larr;</button>
             <img className="cover" src={src}/>
-            <button onClick={changeSlides}>Switch</button>
+            <button onClick={nextSlide} className="arrows nextArrow">&rarr;</button>
         </div>
     )
 }
